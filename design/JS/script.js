62a50b72a2e61dca5e4d35b565e80ea3d53ac7d6
Tetsuya Doko <tetsu.dowell@gmail.com>
2026-08-17 18:36:01 +0900

    デザイン変更（ハンバーガーと共通CSS

--- design/JS/script.js
+++ design/JS/script.js
@@ -1,6 +1,11 @@
+
 const hamburger = document.querySelector('.hamburger');
 const nav = document.querySelector('.global-nav');
+const overlay = document.querySelector('.menu-overlay');
 
-hamburger.addEventListener('click', function () {
-    nav.classList.toggle('active');
+
+hamburger.addEventListener('click', () => {
+nav.classList.toggle('open');
+hamburger.classList.toggle('active');
+overlay.classList.toggle('open');
 });
